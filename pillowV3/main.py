a77fa577248b9a08a615cb13b65ec54df7df564e
WojciechKoz <kozlowski.wojciech092@gmail.com>
2018-12-29 19:18:07 +0100

    many batches (50 x 100 elements)

--- pillowV3/main.py
+++ pillowV3/main.py
@@ -4,9 +4,12 @@ from mnist import MNIST
 
 mndata = MNIST('./data')
 images_full, labels_full = mndata.load_training()
+images = []
+labels = []
 
-images = images_full[:100]
-labels = labels_full[:100]
+for i in range(50):
+    images.append(images_full[i*100 : (i+1)*100])
+    labels.append(labels_full[i*100 : (i+1)*100])
 
 def sigmoid_prime(x):
     return np.exp(-x) / ((np.exp(-x) + 1) ** 2)
@@ -20,11 +23,14 @@ def sigmoid(x):
 #              [1, 0],
 #              [1, 1]])
 
-X = np.array(images)
+#X = np.array(images)
 
 y = []
-for label in labels:
-    y.append([1.0 if i == label else 0.0 for i in range(10)])
+
+for batch in labels:
+    y.append([])
+    for label in batch:
+        y[-1].append([1.0 if i == label else 0.0 for i in range(10)])
 
 y = np.array(y)
 
@@ -45,24 +51,29 @@ syn1 = 2 * np.random.random((SIZES[1], SIZES[2])) - 1
 b0 = 2 * np.random.random((1, SIZES[1])) - 1
 b1 = 2 * np.random.random((1, SIZES[2])) - 1
 
-for j in range(500):
-    l0 = X
-    l1 = sigmoid(np.dot(l0, syn0) + b0)
-    l2 = sigmoid(np.dot(l1, syn1) + b1)
+for i, batch in enumerate(images):
+    X = np.array(batch)
+    print("x:")
+    print(np.shape(X))
 
-    l2_error = (y - l2)#** 2
+    for j in range(100):
+        l0 = X
+        l1 = sigmoid(np.dot(l0, syn0) + b0)
+        l2 = sigmoid(np.dot(l1, syn1) + b1)
 
-    print(("[%d] error: " % j) + str(np.mean(np.abs(l2_error))))
+        l2_error = (y[i] - l2)#** 2
 
-    l2_delta = l2_error * sigmoid_prime(l2)
-    l1_error = l2_delta.dot(syn1.T)
-    l1_delta = l1_error * sigmoid_prime(l1)
+        print(("[%d] error: " % j) + str(np.mean(np.abs(l2_error))))
 
-    syn1 += l1.T.dot(l2_delta)
-    syn0 += l0.T.dot(l1_delta)
+        l2_delta = l2_error * sigmoid_prime(l2)
+        l1_error = l2_delta.dot(syn1.T)
+        l1_delta = l1_error * sigmoid_prime(l1)
 
-    b0 += l1_delta.mean(axis=0)
-    b1 += l2_delta.mean(axis=0)
+        syn1 += l1.T.dot(l2_delta)
+        syn0 += l0.T.dot(l1_delta)
+
+        b0 += l1_delta.mean(axis=0)
+        b1 += l2_delta.mean(axis=0)
 
 
 def predict(data):
@@ -74,7 +85,7 @@ def predict(data):
 print("Output after training: ")
 print(l2)
 for i, el in enumerate(l2):
-    print(labels[i], "=", np.argmax(el), " predictions: ", el)
+    print(labels[0][i], "=", np.argmax(el), " predictions: ", el)
 
-for i in range(101, 200):
+for i in range(5000, 5100):
     print(labels_full[i], predict(images_full[i]))
